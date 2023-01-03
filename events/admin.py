d9b7e23fa0df839055732ea0c572d8e886ca8626
Andy Guttridge <andyguttridge123@gmail.com>
2023-01-03 11:44:28 +0000

    Feature: add events model to  admin panel

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    """
+    Specify fields to be accesible in admin panel for Profile model
+    """
+    list_display = (
+        'user',
+        'tribe',
+        'get_to',
+        'start',
+        'duration',
+        'recurrence_type',
+        'subject',
+        'category',
+        'accepted',
+    )
+
+    def get_to(self, obj):
+        """
+        Create a string of the users invited to this event
+        """
+        # Approach to creating a string representation of a many-to-many field
+        # adapted from
+        # https://stackoverflow.com/questions/18108521/many-to-many-in-list-display-django
+        string = ''
+        for user in obj.profile.all():
+            string += str(profile.user)
+        return string
