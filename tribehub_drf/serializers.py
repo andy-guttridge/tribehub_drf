8820dabd153be46fea8b3729ba5f662df301c130
Andy Guttridge <andyguttridge123@gmail.com>
2023-01-04 14:43:55 +0000

    Feature: add user details serializer

---
+++ tribehub_drf/serializers.py
@@ -0,0 +1,12 @@
+from dj_rest_auth.serializers import UserDetailsSerializer
+from rest_framework import serializers
+
+
+class CurrentUserSerializer(UserDetailsSerializer):
+    """
+    Serialize current user data. Include profile image and user id.
+    """
+    profile_image = serializers.ReadOnlyField(source='profile.image.url')
+
+    class Meta(UserDetailsSerializer.Meta):
+        fields = UserDetailsSerializer.Meta.fields + 'profile_image'
