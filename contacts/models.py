dada4badb921e7b924dee856874613856de0dca8
Andy Guttridge <andyguttridge123@gmail.com>
2023-01-11 17:23:01 +0000

    Fix: fix incorrectly specified email field

--- contacts/models.py
+++ contacts/models.py
@@ -13,4 +13,4 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
-    email = models.EmailField
+    email = models.EmailField(null=True)
