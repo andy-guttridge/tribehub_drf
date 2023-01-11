0421ae7141028473e216d79cf073468285dac2ca
Andy Guttridge <andyguttridge123@gmail.com>
2023-01-11 16:54:19 +0000

    Fix: remove unecessary field declaration from notifications serializer

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -9,8 +9,6 @@ class NotificationSerializer(serializers.ModelSerializer):
     """
     Serializer for notifications
     """
-    date_created = serializers.DateTimeField(format='iso-8601', default_timezone=None)
-
     class Meta:
         model = Notification
         fields = [
