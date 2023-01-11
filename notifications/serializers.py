7c6c62c66192638e8e857971b3fffe9a81f5ebed
Andy Guttridge <andyguttridge123@gmail.com>
2023-01-11 16:33:41 +0000

    Fix: add id field to notifications serializer

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -14,6 +14,7 @@ class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
         fields = [
+            'id',
             'to_user',
             'date_created',
             'subject',
