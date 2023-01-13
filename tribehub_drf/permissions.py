7d8f6cc5486eda4023a922644c6522a62195839a
Andy Guttridge <andyguttridge123@gmail.com>
2023-01-13 14:55:43 +0000

    Refactor: remove unncessary imports

--- tribehub_drf/permissions.py
+++ tribehub_drf/permissions.py
@@ -1,7 +1,4 @@
-from rest_framework import status
 from rest_framework.permissions import BasePermission, SAFE_METHODS
-from rest_framework.response import Response
-from django.contrib.auth.models import User
 from profiles.models import Profile
 
 
