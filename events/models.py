0b0b5fed2829205fab0e44d250e4443f2ed7bd22
Andy Guttridge <andyguttridge123@gmail.com>
2023-01-13 14:39:40 +0000

    Refactor: remove unnecessary imports

--- events/models.py
+++ events/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
 from recurrence.fields import RecurrenceField
 import recurrence
