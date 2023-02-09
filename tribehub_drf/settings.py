101710a437c0040a52b0d0ae5ea70bd9790af198
Andy Guttridge <andyguttridge123@gmail.com>
2023-02-09 16:27:37 +0000

    Config: increase max pagination page size to very large number

--- tribehub_drf/settings.py
+++ tribehub_drf/settings.py
@@ -52,7 +52,7 @@ REST_FRAMEWORK = {
     )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 999999999,
     'DATETIME_FORMAT': 'iso-8601',
     'DATETIME_INPUT_FORMATS': ['iso-8601']
 }
