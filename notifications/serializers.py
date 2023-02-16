968cdb8be0fbc8ec790930478202e7fae7a2a935
Andy Guttridge <andyguttridge123@gmail.com>
2023-02-16 14:57:54 +0000

    Feature: add event data to notifications

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -3,12 +3,15 @@ from django.utils.dateformat import format
 from django.conf import settings
 
 from .models import Notification
+from events.serializers import EventSerializer
 
 
 class NotificationSerializer(serializers.ModelSerializer):
     """
     Serializer for notifications
     """
+    event = EventSerializer()
+
     class Meta:
         model = Notification
         fields = [
